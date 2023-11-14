4103d21a9bebf677966830f857e94e62b0657d42
Herold7 <makpier@gmail.com>
2023-11-14 16:58:27 +0100

    📝 Ajout de code pour le design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Stack, Box, Fab, Button, Typography } from '@mui/material/';
 import './App.css'
 
 function App() {
@@ -8,26 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Typography variant="subtitle" textTransform={"uppercase"} fontSize='900' >Caractéristiques</Typography>
+      <Typography variant="h1" fontWeight={600} color='secondary'>Plus de puissance pour plus d'efficacité</Typography>
+      <Stack direction="row" spacing={2} paddingTop={5}>
+        <Box sx={{ width: '50%', height: 450, borderRadius: '1.5vw 4vw', bgcolor: 'primary.main' }}></Box>
+        <Stack width="50%" spacing={2}>
+          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: 'error.main' }}></Box>
+          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: 'success.main' }}></Box>
+        </Stack>
+      </Stack>
     </>
   )
 }
